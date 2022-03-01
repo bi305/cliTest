e328a2fc8a845faa67f8f0cca5f6571625617806
bi305 <[razab0715@gmail.com]>
2022-03-01 16:42:14 +0500

    login file is  created

---
+++ login.js
@@ -0,0 +1,8 @@
+// compare two arrays
+function compare(a, b) {
+    if (a.length != b.length) return false;
+    for (var i = 0; i < a.length; i++) {
+        if (a[i] != b[i]) return false;
+    }
+    return true;
+}
